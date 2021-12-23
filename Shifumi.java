128114d92b6f2e910e0676147f564b2b6e722d66
Tinoux <valentin.das@sfr.fr>
2021-12-23 20:56:32 +0100

    Mini update 23/12

--- Shifumi.java
+++ Shifumi.java
@@ -1,27 +1,47 @@
 class Shifumi extends EpreuvesCreator {
     
     Epreuve initShiFuMi() {
-        return newEpreuve(4, "ShiFuMi", -1, "Choisis soit la pierre, le papier ou les ciseaux. La pierre bat les ciseaux, les ciseaux battent le papier et le papier bat la pierre.", GameState.JUGEMENT);
+        return newEpreuve(4, "ShiFuMi", -1, "Choisis soit la pierre, le papier ou les ciseaux. La pierre bat les ciseaux, les ciseaux battent le papier et le papier bat la pierre. Le premier qui gagne 3 manches remporte la victoire !", GameState.JUGEMENT);
     }
 
     // A FINIR
     boolean startShiFuMi(Epreuve epreuve, Game game) {
-        println("Quel coup choisis-tu ?");
-        println("1 - Pierre");
-        println("2 - Ciseaux");
-        println("3 - Feuille");
-        int res;
-        do {
-            res = enterNumber();
-        } while(!isBetween(res, 1, 3));
-        int r = randInt(1, 3);
-        if(r == res) {
-            wonEpreuve(game);
-        } else {
-            lostEpreuve(game);
+        int[] winLose = new int[]{0, 0};
+        int res; // joueur
+        int r; // robot
+        while(shiFuMiFinish(winLose)) {
+            println("Manches gagnées : " + winLose[0] + " | Manches perdues : " + winLose[1]);
+            printCoups();
+            do {
+                res = enterNumber();
+            } while(!isBetween(res, 1, 3));
+            r = randInt(1, 3);
+            updateWinLose(winLose, res, r);
         }
         delay(5000);
         return true;
+    }
+
+    boolean shiFuMiFinish(int[] winLose) {
+        return winLose[0] == 3 || winLose[1] == 3;
+    }
+
+    void updateWinLose(int[] winLose, int player, int robot) {
+        if(player == robot) {
+
+        }
+        else if((player == 1 && robot == 2) || (player == 2 && robot == 3) || (player == 3 && robot == 1)) {
+            winLose[0] = winLose[0] + 1;
+        } else {
+            winLose[1] = winLose[1] + 1;
+        }
+    }
+
+    void printCoups() {
+        println("Quel coup choisis-tu ?");
+        println("1 - Pierre");
+        println("2 - Ciseaux");
+        println("3 - Papier");
     }
 
     void wonEpreuve(Game game){
