f6f22082b2824378feebec0157eff7674f0885dc
Tinoux <valentin.das@sfr.fr>
2021-12-30 00:22:51 +0100

    Debut PipeGame

--- src/PipeGame.java
+++ src/PipeGame.java
@@ -1,5 +1,7 @@
 class PipeGame extends Fakir {
 
+    
+
     Epreuve initPipeGame() {
         return newEpreuve(1, "PipeGame", 40, "Pour résoudre ce puzzle vous devez relier les 2 extrémités des tuyaux pour faire circuler l'énergie avant que le temps ne soit écoulé.", GameState.KEYS);
     }
@@ -15,15 +17,13 @@ class PipeGame extends Fakir {
     // 0 -> VIDE
     // 1 -> START
     // 2 -> FIN
-    // 3 -> L -> NE
-    // 4 -> L -> SE
-    // 5 -> L -> SW
-    // 6 -> L -> NW
-    // 7 -> DROIT -> horizontale
-    // 8 -> DROIT -> verticale
-    // 9 -> CROIX
-    // 10 -> T -> N
-    // 11 -> T -> E
-    // 12 -> T -> S
-    // 13 -> T -> W
+    // 3 -> L
+    // 4 -> DROIT
+    // 5 -> CROIX
+    // 6 -> T
+
+    // N
+    // E
+    // S
+    // W
 }
