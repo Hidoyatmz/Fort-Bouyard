96a357ca612605f9aa873980fd65aba5c5968ef3
Tinoux <valentin.das@sfr.fr>
2021-12-31 13:40:56 +0100

    Initialisation PipeGame

--- src/PipeGame.java
+++ src/PipeGame.java
@@ -8,16 +8,21 @@ class PipeGame extends Fakir {
         return newEpreuve(1, "PipeGame", 40, "Pour résoudre ce puzzle vous devez relier les 2 extrémités des tuyaux pour faire circuler l'énergie avant que le temps ne soit écoulé.", GameState.KEYS);
     }
 
+    // DECODAGE MAP
+    // 1er ligne : hauteur largeur
+    // 0 -> VIDE
+    // 1 -> START  ⯌
+    // 2 -> FIN    ⊔ ⊏ ⊓ ⊐
+    // 3 -> L      ╚ ╔ ╗ ╝             ┗ ┏ ┓ ┛
+    // 4 -> DROIT  ═ ║                 ━ ┃
+    // 5 -> CROIX  ╬                   ╋
+    // 6 -> T      ╩ ╠ ╦ ╣             ┻ ┣ ┳ ┫
+    //             N E S W
+
     boolean startPipeGame(Epreuve epreuve, Game game) {
         File[] maps = randomMaps();
-        //Pipe[][] plateau = initPlateau(maps[0]);
-        String[][] map = loadMapFromFile(maps[0]);
-        for(int i=0; i<length(map, 1); i++) {
-            for(int j=0; j<length(map, 2); j++) {
-                print(map[i][j]);
-            }
-            print("\n");
-        }
+        Pipe[][] plateau = initPlateau(maps[0]);
+        afficherMap(plateau);
         readString();
         return true;
     }
@@ -34,16 +39,49 @@ class PipeGame extends Fakir {
     }
 
     void afficherMap(Pipe[][] plateau) {
-        // DECODAGE MAP
-        // 1er ligne : hauteur largeur
-        // 0 -> VIDE
-        // 1 -> START  ⯌
-        // 2 -> FIN    ⊔ ⊏ ⊓ ⊐
-        // 3 -> L      ╚ ╔ ╗ ╝             ┗ ┏ ┓ ┛
-        // 4 -> DROIT  ═ ║                 ━ ┃
-        // 5 -> CROIX  ╬                   ╋
-        // 6 -> T      ╩ ╠ ╦ ╣             ┻ ┣ ┳ ┫
-        //             N E S W
+        String res = "";
+        for(int i=0; i<length(plateau, 1); i++) {
+            for(int j=0; j<length(plateau, 2); j++) {
+                if(plateau[i][j].bloquer) res = res + ANSI_RED_BG;
+                if(plateau[i][j].energie) res = res + ANSI_YELLOW;
+                res = res + getStringPipe(plateau[i][j]) + ANSI_RESET;
+            }
+            res = res + "\n";
+        }
+    }
+
+    String getStringPipe(Pipe pipe) {
+        String res;
+        if(pipe.pipeType == PipeType.START) res = "⯌";
+        else if(pipe.pipeType == PipeType.END) {
+            if(pipe.direction == Direction.N) res = "⊔";
+            else if(pipe.direction == Direction.E) res = "⊏";
+            else if(pipe.direction == Direction.S) res = "⊓";
+            else res = "⊐";
+        }
+        else if(pipe.pipeType == PipeType.L) {
+            if(pipe.direction == Direction.N) res = "┗";
+            else if(pipe.direction == Direction.E) res = "┏";
+            else if(pipe.direction == Direction.S) res = "┓";
+            else res = "┛";
+        }
+        else if(pipe.pipeType == PipeType.DROIT) {
+            if(pipe.direction == Direction.N || pipe.direction == Direction.S) res = "┃";
+            else res = "━";
+        }
+        else if(pipe.pipeType == PipeType.CROIX) {
+            res = "╋";
+        }
+        else if(pipe.pipeType == PipeType.T) {
+            if(pipe.direction == Direction.N) res = "┻";
+            else if(pipe.direction == Direction.E) res = "┣";
+            else if(pipe.direction == Direction.S) res = "┳";
+            else res = "┫";
+        }
+        else {
+            res = " ";
+        }
+        return res;
     }
 
     Pipe[][] initPlateau(File file) {
